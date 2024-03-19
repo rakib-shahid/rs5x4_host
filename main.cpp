db2983c5dc8a418f33e6145b12aef5801cbd6679
rakib-shahid <rakibshahid1298@gmail.com>
2024-03-18 23:16:25 -0400

    idk what im doing

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <hidapi.h>
+using namespace std;
+
+int main(){
+    int res;
+	unsigned char buf[65];
+	wchar_t wstr[255];
+	hid_device *handle;
+	int i;
+
+	// Initialize the hidapi library
+	res = hid_init();
+
+	// Open the device using the VID, PID,
+	// and optionally the Serial number.
+	handle = hid_open(0x4d8, 0x3f, NULL);
+	if (!handle) {
+		printf("Unable to open device\n");
+		hid_exit();
+ 		return 1;
+	}
+
+	// Read the Manufacturer String
+	res = hid_get_manufacturer_string(handle, wstr, 255);
+	printf("Manufacturer String: %ls\n", wstr);
+    return 0;
+}
