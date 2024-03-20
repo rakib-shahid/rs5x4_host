8306044cd575decd00cab3db4096fa5773cb8f65
rakib-shahid <rakibshahid1298@gmail.com>
2024-03-20 13:38:07 -0400

    finally sending hid messages properly

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,47 @@ int main(int argc, char *argv[]){
 	wchar_t wstr[255];
 	hid_device *handle;
     hid_device_info *device_info;
-	int i;
+    int vid = 0xFEDD;
+    int pid = 0x0753;
 
 	// Initialize the hidapi library
 	res = hid_init();
 
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
-    device_info = hid_enumerate(0xFEDD, 0x0753);
-	handle = hid_open(0xFEDD, 0x0753, NULL);
+	handle = hid_open(vid, pid, NULL);
 	if (!handle) {
 		printf("Unable to open device\n");
 		hid_exit();
  		return 1;
 	}
-    // printf("%s",device_info->product_string);
 	// Read the Manufacturer String
     res = hid_get_product_string(handle, wstr, 255);
     printf("Product String: %ls\n", wstr);
 	res = hid_get_manufacturer_string(handle, wstr, 255);
 	printf("Manufacturer String: %ls\n", wstr);
     unsigned char buf[32];
-    buf[0] = 0x00;
-    buf[1] = 0xFF;
+    buf[0] = 0x0;
+    buf[1] = 0xFB;
     buf[2] = 1;
+
+
+    hid_device *device;
+    hid_device_info *first;
+    hid_device_info *info;
+    int usage_page = 0xFF60;
+    int usage = 0x61;
+    first = hid_enumerate(vid, pid);
+    info = first;
+    while (info) {
+        if (info->usage_page == usage_page && info->usage == usage) {
+            device = hid_open_path(info->path);
+            break;
+        }
+        info = info->next;
+    }
+    hid_free_enumeration(first);
+
     if (argc > 1){
         cout << argv[1] << endl;
         char* test = argv[1];
@@ -42,7 +59,11 @@ int main(int argc, char *argv[]){
         for (unsigned char x: buf){
             cout << x << " ";
         }
-        hid_write(handle,buf,32);
+        int result = hid_write(device,buf,32);
+        printf("\nresult = %d",result);
+        if (result == -1){
+            printf("\n%ls\n",hid_error(device));
+        }
     }
     return 0;
 }
