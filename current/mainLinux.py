c6cd4413a066acc76cd7c5d82d4685f33ea6526f
rakib-shahid <rakibshahid1298@gmail.com>
2024-04-20 13:36:27 -0400

    linux

---
+++ current/mainLinux.py
@@ -0,0 +1,395 @@
+from math import floor
+import subprocess
+import time
+from typing import Iterable
+import spotifykeys
+from PIL import Image
+import requests
+import spotipy
+# import wmi
+import hid
+# import colorsys
+import os
+import re
+import threading
+
+# import math
+###################################### 
+# spotify setup
+SPOTIPY_REDIRECT_URI='https://localhost:8888/callback'
+
+auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=spotifykeys.client_id, client_secret=spotifykeys.client_secret, redirect_uri= SPOTIPY_REDIRECT_URI,scope='user-read-currently-playing', show_dialog=True, cache_path='./tokens.txt')
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+track_info = {
+    "is_playing": False,
+    "track_name": '',
+    "artist_name": '',
+    "image_url": '',
+    "track_progress": 0
+}
+old_track_info = {
+    "is_playing": False,
+    "track_name": '',
+    "artist_name": '',
+    "image_url": '',
+    "track_progress": 0
+}
+# spotify function
+def get_current_track_info():
+    # start = time.process_time()
+    try:
+        response = spotify.current_user_playing_track()
+        # print("Total time taken to getCurrentTrack = "+ str(time.process_time() - start))
+        track_info["is_playing"] = response["is_playing"]
+        track_info["track_name"] = response["item"]["name"]
+        track_info["artist_name"] = response["item"]["artists"][0]["name"]
+        track_info["image_url"] = response["item"]["album"]["images"][2]["url"]
+        track_info["track_progress"] = floor(response["progress_ms"] / response["item"]["duration_ms"] * 128)
+    except TypeError:
+        track_info["is_playing"] = False
+        track_info["track_name"] = ""
+        track_info["artist_name"] = ""
+        track_info["image_url"] = ""
+        track_info["track_progress"] = 0
+#####################################################
+# hid setup
+vendor_id     = 0xFEDD
+product_id    = 0x0753
+
+usage_page    = 0xFF60
+usage         = 0x61
+report_length = 32
+# hid functions
+def get_raw_hid_interface():
+    device_interfaces = hid.enumerate(vendor_id, product_id)
+    raw_hid_interfaces = [i for i in device_interfaces if i['usage_page'] == usage_page and i['usage'] == usage]
+
+    if len(raw_hid_interfaces) == 0:
+        return None
+
+    interface = hid.Device(path=raw_hid_interfaces[0]['path'])
+
+    # print(f"Manufacturer: {interface.manufacturer}")
+    # print(f"Product: {interface.product}")
+
+    return interface
+
+def send_raw_report(op,data):
+    interface = get_raw_hid_interface()
+
+    if interface is None:
+        raise FileNotFoundError
+
+    request_data = [0x00] * (report_length - 3) # First byte is Report ID
+    request_data[1] = op
+    if op == 0xFA:
+        request_data[2] = data
+        if (track_info["track_progress"] < old_track_info["track_progress"]):
+            request_data[3] = 0
+        else:
+            request_data[3] = 1
+    else:
+        request_data[3:len(str(data))] = data
+        if (track_info["is_playing"]):
+            request_data[2] = 1
+        else:
+            request_data[2] = 0
+        # print(f"len(request_data) = {len(request_data)}")
+    request_report = bytes(request_data)    
+        
+    
+    # print(f"request_data = {request_data}")
+    # print(f"Request = {request_report}")
+
+    try:
+        interface.write(request_report)
+
+        response_report = interface.read(report_length, timeout=1000)
+
+        # print("Response:")
+        # print(response_report)
+    except Exception as e:
+        print(e)
+#####################################################
+# image functions
+def send_image_data(data,first = None,last=None):
+    # send_raw_report(0xCC,data,offset1,offset2)
+    # print(offset)
+    interface = get_raw_hid_interface()
+
+    if interface is None:
+        print("No device found")
+        raise FileNotFoundError
+
+    request_data = [0x00] * (report_length - 4) # First byte is Report ID
+    if (first == True):
+        request_data[1] = 0xFD
+    elif (last == True):
+        request_data[1] = 0xFC
+    else:
+        request_data[1] = 0xFE
+    request_data[2:len(data)] = data
+    # print(f"len(request_data) = {len(request_data)}")
+    print(f"image_data = {request_data}")
+    print(f"Raw bytes = {bytes(request_data)}")
+
+    try:
+        interface.write(bytes(request_data))
+
+        # response_report = interface.read(report_length, timeout=1000)
+    except Exception as e:
+        print(e)
+    
+
+def rgb_to565(r, g, b):
+    msb = ((r >> 3 & 0x1F) << 3) + (g >> 5 & 0x07)
+    lsb = ((g >> 2 & 0x07) << 5) + (b >> 3 & 0x1F)
+    return [msb, lsb]
+
+
+class NoDeviceException(Exception):
+    """Custom excption for reporting errors"""
+
+class ImageSender:
+    def __init__(self, url: str):
+        """Create the 'sender' by storing the Pillow Image and its QMK'ized data"""
+        _image = Image.open(requests.get(url,stream=True).raw).resize((64,64)).convert('RGB')
+
+        _buffer = []
+        # f = open('fsf.txt','a')
+        
+        for col in range(_image.width):
+            for row in range(_image.height):
+                rgb = _image.getpixel((col, row))
+                fsf = rgb_to565(*rgb) 
+                # f.write(str(fsf))
+                _buffer.append(fsf)
+        # f.close()
+        # for i in range(65):
+        #     # Insert black pixels to the end of each row
+        #     _buffer.append(rgb_to565(0,0,0) )
+
+        self._image = _image
+        self._buffer = _buffer
+
+
+    def _chunks(self, bytes_per_chunk: int) -> Iterable[list[int]]:
+        """Get the raw data in chunks ready to be sent"""
+        for offset in range(0, len(self._buffer), bytes_per_chunk):
+            # NOTE: last item may be smaller
+            yield self._buffer[offset:offset + bytes_per_chunk]
+    def send(self, drawing_offset: tuple[int, int] = (0, 0)):
+        """Method to send"""
+
+        interface = get_raw_hid_interface()
+        # print(interface)
+        if interface is None:
+            raise NoDeviceException
+
+        # TODO: implement a custom message to execute `qp_set_viewport` into the desired range
+        # viewport(drawing_offset, self._image.size)
+
+        # -- Send actual pixels
+        # NOTE: This relies on your firmware not drawing while you are sending, it would mess up the viewport
+
+        # set up message header
+        request = bytearray([0] * report_length)
+        request[1] = 0xFE
+        n = 30  # whatever this ends up being
+
+        # send data re-using the buffer
+        for chunk in self._chunks(n):
+            request[2] = len(chunk)
+            request[2:2 + len(chunk)] = chunk
+            # print(request)
+
+            try:
+                interface.write(bytes(request))
+                response_report = interface.read(report_length, timeout=1000)
+            finally:
+                interface.close()
+
+
+def get_image_data_new(image_url):
+    sender  = ImageSender(image_url)
+    
+    image_buffer = sender._buffer
+    columns = []
+    chunk = []
+    all_data = []
+    for i in range(len(image_buffer)):
+        all_data.extend(image_buffer[i])
+        chunk.extend(image_buffer[i])
+        if (len(chunk) == 128):
+            columns.append(chunk)
+            chunk = []
+    
+    # print()
+    hid_messages = []
+    hidmessages = []
+    i = 0
+    # print(all_data)
+    f = open('allinfo.txt', 'w')
+    f.write(str(all_data).replace('[','').replace(']',''))
+    while i < len(all_data):
+        # f.write(str(all_data[i:i+30]))
+        hid_messages.append(all_data[i:i+30])
+        i += 30
+    f.close()
+    # for column in columns:
+    #     hidmessages.append(column[:30])
+    #     hidmessages.append(column[30:60])
+    #     hidmessages.append(column[60:90])
+    #     hidmessages.append(column[90:120])
+    #     hidmessages.append(column[120:])
+    # print(len(hid_messages))
+    # print(len(hid_messages[0]))
+    # print(hid_messages)
+    return hid_messages
+
+
+
+
+
+###################################
+# IMPORTANT FUNCTION
+def cycleString(input_string, i):
+    if len(input_string) > 18:
+        start_index = i % len(input_string)
+        substring = input_string[start_index:] + input_string[:start_index]
+        result = substring[:18]
+    else:
+        result = input_string
+    return result
+
+outString = ''
+lastString = ''
+cycled = ''
+i = 1
+apiTimer = 0
+
+
+
+
+###################################
+# IMPORTANT FUNCTION
+def image_hid():
+    
+    hidmessages = get_image_data_new(track_info["image_url"])  
+    i = 0
+    
+    while (track_info["is_playing"] and len(threads) == 1 and i < len(hidmessages)):
+        if i == 0:
+            send_image_data(bytes(hidmessages[i]),first=True)
+        if i == len(hidmessages) - 1:
+            send_image_data(bytes(hidmessages[i]),last=True)
+        else:
+            send_image_data(bytes(hidmessages[i]))
+        i+= 1
+    # subprocess.call('./sendlinux',stdout=subprocess.DEVNULL)
+    threads.clear()
+    # print("image_hid thread done")
+
+###################################
+# IMPORTANT FUNCTION
+def check_song():
+    global song_changed
+    global threads
+    while True:
+        # print(track_info)
+        try:
+            try:
+                get_current_track_info()
+                # print(f'progress = {track_info["track_progress"]}')
+            except Exception as e:
+                print(e)
+                print("getting new token")
+                auth_manager = spotipy.oauth2.SpotifyOAuth(spotifykeys.client_id, spotifykeys.client_secret, redirect_uri= SPOTIPY_REDIRECT_URI,scope='user-read-currently-playing', show_dialog=True)
+                spotify = spotipy.Spotify(auth_manager=auth_manager)
+                get_current_track_info()
+            if not (track_info["image_url"] == old_track_info["image_url"]):
+                
+                if (len(threads) == 0):
+                    image_thread = threading.Thread(target=image_hid)
+                    image_thread.start()
+                    threads.append(image_thread)
+                else:
+                    threads.append(1)
+                    time.sleep(.05)
+                    image_thread = threading.Thread(target=image_hid)
+                    image_thread.start()
+                    threads.append(image_thread)
+                    
+                
+                
+                
+            time.sleep(0.5)
+            old_track_info["is_playing"] = track_info["is_playing"]
+            old_track_info["track_name"] = track_info["track_name"]
+            old_track_info["artist_name"] = track_info["artist_name"]
+            old_track_info["image_url"] = track_info["image_url"]
+        except TypeError:
+            track_info["is_playing"] = False
+            track_info["track_name"] = ""
+            track_info["artist_name"] = ""
+            track_info["image_url"] = ""
+
+threads = []
+song_changed = threading.Event()
+song_change_thread = threading.Thread(target=check_song)
+song_change_thread.daemon = True
+song_change_thread.start()
+
+###################################
+# IMPORTANT FUNCTION
+def clean_song_string(song_string):
+    # og_regex = r'[^\w\s\'’`!@#$%^&*()_+\-=\[\]{};\\:"|,.<>\/?♫]'
+    og_regex = r'[^\x00-\x7F\♫]'
+    cleaned_string = re.sub(og_regex, '?', song_string)
+    # print(cleaned_string)
+    return cleaned_string
+
+idle_sent = False
+redraw = False
+while True:
+    try:
+        
+        if (track_info["is_playing"]):
+            if idle_sent:
+                idle_sent = False
+            outString = f'♫ {(track_info["artist_name"])} - {(track_info["track_name"])} '
+            # print(clean_song_string(outString))
+            if not (outString == lastString):
+                i = 1
+                send_raw_report(0xFF, bytes(clean_song_string(outString[:18]),'utf-8'))
+                cycled = outString
+            else:
+                if not redraw:
+                    # print("redrew")
+                    send_raw_report(0xFB,bytes("redraw",'utf-8'))
+                    redraw = True
+                if len(outString) > 18:
+                    send_raw_report(0xFF,bytes(clean_song_string(cycleString(outString,i)[:18]),'utf-8'))
+                    i += 1
+                else:
+                    send_raw_report(0xFF, bytes(clean_song_string(cycleString(outString,i)[:18]),'utf-8'))
+            send_raw_report(0xFA,track_info["track_progress"])
+                
+        else:
+            redraw = False
+            if not idle_sent:
+                send_raw_report(0xFF, bytes("",'utf-8'))
+                idle_sent = True
+                print("Not playing hid message sent")
+
+            i = 0
+        apiTimer += 1
+        time.sleep(0.33)
+        lastString = outString
+        
+    except FileNotFoundError:
+        pass
+    except hid.HIDException:
+        print("unable to open device")
+    
--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,10 @@ int main(int argc, char *argv[]){
 
 	// Initialize the hidapi library
 	res = hid_init();
-
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	handle = hid_open(vid, pid, NULL);
-	if (!handle) {
-		printf("Unable to open device\n");
-		hid_exit();
- 		return 1;
-	}
     
-	// Read the Manufacturer String
-    // res = hid_get_product_string(handle, wstr, 255);
-    // printf("Product String: %ls\n", wstr);
-	// res = hid_get_manufacturer_string(handle, wstr, 255);
-	// printf("Manufacturer String: %ls\n", wstr);
-    // unsigned char buf[32];
-    // buf[0] = 0x0;
-    // buf[1] = 0xFB;
-    // buf[2] = 1;
+    
+    
+	
 
 
 
@@ -46,17 +31,27 @@ int main(int argc, char *argv[]){
     int usage = 0x61;
     first = hid_enumerate(vid, pid);
     info = first;
+    printf("enumerating\n");
     while (info) {
+        printf("Name: %ls, Usage: %d, Usage Page: %d\n", info->manufacturer_string, info->usage, info->usage_page);
         if (info->usage_page == usage_page && info->usage == usage) {
+            printf("found hid device\n");
             device = hid_open_path(info->path);
             break;
         }
         info = info->next;
     }
+    cout << "device is: "<< device <<endl;
     hid_free_enumeration(first);
 
+    // Read the Manufacturer String
+    res = hid_get_product_string(device, wstr, 255);
+    printf("Product String: %ls\n", wstr);
+	res = hid_get_manufacturer_string(device, wstr, 255);
+	printf("Manufacturer String: %ls\n", wstr);
+
     const int ARRAY_SIZE = 32; // Length of the array
-    unsigned char array[ARRAY_SIZE]; // Array to store integers
+    unsigned char array[32]; // Array to store integers
 
     // Open the file
     ifstream file("allinfo.txt");
@@ -71,10 +66,10 @@ int main(int argc, char *argv[]){
     int index = 2;
     int totalIntegers = 0;
     bool isFirstArray = true;
+    int messagesSent = 0;
 
     // Add 0x0 to the first position of the array
     array[0] = 0x0;
-
     while (getline(file, line, ',')) { // Read each integer separated by comma
         if (isFirstArray) {
             array[1] = 0xFD;
@@ -84,23 +79,24 @@ int main(int argc, char *argv[]){
         }
         array[index++] = stoi(line); // Convert line to integer and store in the array
         ++totalIntegers;
-
         if (index == ARRAY_SIZE) { // If array is filled
             // Call hid_send function with the array
+            cout << device << endl;
+            cout << &array << endl;
             hid_write(device,array,32);
+            hid_error(device);
 
             // Reset index for next array
             index = 2;
         }
     }
-
     // Check if there are remaining integers
     if (index > 2) {
         array[1] = 0xFC;
         // Call hid_send with the remaining integers
         hid_write(device,array,32);
+        
     }
-    
     // Close the file
     file.close();
     return 0;
